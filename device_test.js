b579e4292b2abcbfba95b60a94ff0ab13bccfd7f
Lachlan Gill <lachlan.gill@emnify.com>
2019-07-04 11:37:22 +0200

    updated device testing script

--- device_test.js
+++ device_test.js
@@ -1,5 +1,6 @@
 //global variables
 var backlight = true;
+var contrast = 0.5;
 
 // main menu
 var mainmenu = {
@@ -7,21 +8,32 @@ var mainmenu = {
 	"Backlight On" : function() { LED1.set(); },
 	"Backlight Off" : function() { LED1.reset(); },
 	"Config" : function() {Pixl.menu(submenu); },
-	"Exit" : function() { Pixl.menu(); },
+	"Exit" : function() { Pixl.menu(mainmenu); },
 };
 
 // submenu - config
 var submenu = {
 	"" : { "title" : "-- Configuration --" },
-	"Contrast Reset" : function() { Pixl.setContrast(0.5); },
+	"Contrast Increase" : function() { raiseContrast(); },
+	"Contrast Decrease" : function() { lowerContrast(); },
 	"Toggle Backlight" : function() { toggleBacklight(); },
 	"Placeholder" : undefined,
 	"< Back to previous" : function() {Pixl.menu(mainmenu); },
 };
 
+function lowerContrast() {
+	contrast = contrast - 0.1;
+	Pixl.setContrast(contrast);
+}
+
+function raiseContrast() {
+	contrast = contrast + 0.1;
+	Pixl.setContrast(contrast);
+}
+
 function toggleBacklight() {
 	backlight = !backlight;
-	Pixl.setLCDPower(backlight);
+	digitalWrite(LED1,backlight);
 }
 
 function onInit() {
